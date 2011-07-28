b0bbb8b1fe37ae4e355ee11f0f7230399fd40370
yono <yono@www26199u.sakura.ne.jp>
2011-07-28 23:11:22 +0900

    first commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+IrcLogViewer::Application.config.session_store :cookie_store, :key => '_irc_log_viewer_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# IrcLogViewer::Application.config.session_store :active_record_store
